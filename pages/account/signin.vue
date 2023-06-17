e9b96fd79c1d2d95dbcbc8a889fdd2be33b1fb73
mora <you@example.com>
2023-06-17 11:22:05 +0800

    swap password visibility

--- pages/account/signin.vue
+++ pages/account/signin.vue
@@ -33,8 +33,8 @@
                     @keyup.enter="submit"
                     clearable
                     :type="passwordInputType"
-                    :append-inner-icon="passwordInputType === 'password' ? 'visibility' : 'visibility_off'"
-                    @click:append-inner="passwordInputType = passwordInputType === 'password' ? 'text' : 'password'"
+                    :append-inner-icon="passwordInputType === 'password' ? 'visibility_off' : 'visibility'"
+                    @click:append-inner="passwordInputType = passwordInputType === 'password' ? 'password' : 'text'"
                 ></v-text-field>
               </v-form>
 
